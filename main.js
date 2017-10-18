a5412f7df4f9427e52abbb5ba4631b9a285353e4
Katu_brd <katuthings@gmail.com>
2017-10-18 15:07:50 +0200

    more time...

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var four = document.getElementById('ring4');
 
 var ringCarusel = new TimelineLite();
 
-ringCarusel.from(one, 1, {rotation: '-57deg'}, '+=1')
-            .from(two, 1, {rotation: '157deg'})
-            .from(three, 1, {rotation: '-136deg'})
-            .from(four, 1, {rotation: '237deg'});
+ringCarusel.from(one, 1.7, {rotation: '-57deg'}, '+=5')
+            .from(two, 2, {rotation: '157deg'})
+            .from(three, 2, {rotation: '-136deg'})
+            .from(four, 1.5, {rotation: '237deg'});
